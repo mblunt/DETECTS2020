92f07ed4cedad6ece39efcda441e08ecffd8dd54
mblunt <52963601+mblunt@users.noreply.github.com>
2020-11-04 13:20:22 -0500

    Fixed log functionality

--- transfer.sh
+++ transfer.sh
@@ -5,17 +5,17 @@ while [ "$a" -lt 100 ]
 do
 
     #Change Directory
-    cd /home/pi/Programs
-
+    cd ~/Programs
+    
     sleep 1
 
     #make directory
     mkdir /tmp/experiment
 
     #Move files
-    mv ~/home/pi/*.json /tmp/experiment/ 
-    mv ~/home/pi/Programs/*.json /tmp/experiment/ 
+    mv ~/*.json /tmp/experiment/ 
+    mv ~/Programs/*.json /tmp/experiment/ 
 
-    #log
-    date > ~/home/pi/Programs/transfer_log.txt
+    #append current time to log
+    echo "Transfer Program running as of: $(date)." > ~/Programs/transfer_log.txt
 done
